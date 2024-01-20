af20a2772eb107919bd41ad955bf33b7f16ed064
Josua Krause <josua.krause@gmail.com>
2024-01-20 09:45:15 -0500

    redirect from other pages

---
+++ controls.js
@@ -0,0 +1,253 @@
+/**
+ * Copyright 2024 Josua Krause, Christopher Collins
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// @ts-check
+
+import { COLORS, PRESETS } from './presets.js';
+import { attr } from './util.js';
+
+/** @typedef {import('./presets').Preset} Preset */
+/**
+ * @typedef {{
+ *  initPreset: number,
+ *  initIx: number,
+ *  initPad: number,
+ *  minPad: number,
+ *  maxPad: number,
+ *  stepPad: number,
+ *  onPresetUpdate: (preset: Preset) => void,
+ *  onColorUpdate: (ix: number) => void,
+ *  onPadUpdate: (newPad: number) => void,
+ *  onDebugUpdate: (isDebug: boolean) => void,
+ *  onRemoveUpdate: (isRemove: boolean) => void,
+ *  onClear: () => void,
+ * }} Controls
+ */
+
+function addDivider(/** @type {HTMLElement} */ main) {
+  const div = document.createElement('div');
+  div.classList.add('divider');
+  main.appendChild(div);
+}
+
+function addLabel(
+  /** @type {HTMLElement} */ main,
+  /** @type {HTMLElement} */ target,
+  /** @type {string} */ name,
+  /** @type {string} */ desc,
+) {
+  const label = document.createElement('label');
+  label.setAttribute('for', name);
+  label.textContent = desc;
+  main.appendChild(label);
+  attr(target, { id: name, name });
+}
+
+function addPresetSelect(
+  /** @type {HTMLElement} */ main,
+  /** @type {Controls} */ controls,
+) {
+  const div = document.createElement('div');
+  const selectPreset = document.createElement('select');
+  addLabel(div, selectPreset, 'select-preset', 'Preset:');
+  PRESETS.forEach((preset, ix) => {
+    const option = document.createElement('option');
+    option.value = `${ix}`;
+    option.innerText = preset.name;
+    selectPreset.appendChild(option);
+  });
+  selectPreset.addEventListener('change', () => {
+    const newIx = +selectPreset.value;
+    controls.onPresetUpdate(PRESETS[newIx]);
+  });
+  selectPreset.value = `${controls.initPreset}`;
+  div.appendChild(selectPreset);
+  main.appendChild(div);
+  return {
+    updatePreset: (/** @type {number} */ ix) => {
+      if (selectPreset.value !== `${ix}`) {
+        selectPreset.value = `${ix}`;
+      }
+    },
+  };
+}
+
+function addColorSelect(
+  /** @type {HTMLElement} */ main,
+  /** @type {Controls} */ controls,
+) {
+  const div = document.createElement('div');
+  const selectColor = document.createElement('select');
+  addLabel(div, selectColor, 'select-color', 'Color:');
+  COLORS.forEach((_, ix) => {
+    const option = document.createElement('option');
+    option.value = `${ix}`;
+    option.innerText = `Group ${ix + 1}`;
+    selectColor.appendChild(option);
+  });
+  selectColor.addEventListener('change', () => {
+    const newIx = +selectColor.value;
+    selectColor.style.backgroundColor = COLORS[newIx];
+    controls.onColorUpdate(newIx);
+  });
+  selectColor.value = `${controls.initIx}`;
+  selectColor.style.backgroundColor = COLORS[controls.initIx];
+  div.appendChild(selectColor);
+  main.appendChild(div);
+  return {
+    updateColor: (/** @type {number} */ ix) => {
+      if (selectColor.value !== `${ix}`) {
+        selectColor.value = `${ix}`;
+        selectColor.style.backgroundColor = COLORS[ix];
+      }
+    },
+  };
+}
+
+function addRemoveToggle(
+  /** @type {HTMLElement} */ main,
+  /** @type {Controls} */ controls,
+) {
+  const div = document.createElement('div');
+  const toggle = document.createElement('input');
+  addLabel(div, toggle, 'remove', 'Remove:');
+  toggle.setAttribute('type', 'checkbox');
+  toggle.checked = false;
+  toggle.addEventListener('change', () => {
+    controls.onRemoveUpdate(toggle.checked);
+  });
+  div.appendChild(toggle);
+  main.appendChild(div);
+  return {
+    setRemoveMode: (/** @type {boolean} */ isRemove) => {
+      toggle.checked = isRemove;
+    },
+  };
+}
+
+function addPaddingSlider(
+  /** @type {HTMLElement} */ main,
+  /** @type {Controls} */ controls,
+) {
+  const div = document.createElement('div');
+  const range = document.createElement('input');
+  addLabel(div, range, 'pad-range', 'Padding:');
+  const initValue = controls.initPad;
+  const maxValue = controls.maxPad;
+  const minValue = controls.minPad;
+  const step = controls.stepPad;
+  attr(range, {
+    type: 'range',
+    min: `${minValue}`,
+    max: `${maxValue}`,
+    step: `${step}`,
+  });
+  const edit = document.createElement('input');
+  attr(edit, {
+    id: `pad-range-edit`,
+    name: `pad-range-edit`,
+    type: 'text',
+    value: `${initValue}`,
+  });
+  edit.classList.add('rangeedit');
+  edit.value = `${initValue}`;
+  range.value = `${initValue}`;
+  edit.addEventListener('change', () => {
+    const evalue = +edit.value;
+    if (Number.isFinite(evalue)) {
+      controls.onPadUpdate(evalue);
+      range.value = `${evalue}`;
+      edit.classList.remove('invalid');
+    } else {
+      edit.classList.add('invalid');
+    }
+  });
+  range.addEventListener('input', () => {
+    const evalue = +edit.value;
+    const rvalue = +range.value;
+    if (evalue !== rvalue) {
+      controls.onPadUpdate(rvalue);
+      edit.value = `${rvalue}`;
+      edit.classList.remove('invalid');
+    }
+  });
+  div.appendChild(edit);
+  div.appendChild(range);
+  main.appendChild(div);
+  return {};
+}
+
+function addDebugToggle(
+  /** @type {HTMLElement} */ main,
+  /** @type {Controls} */ controls,
+) {
+  const div = document.createElement('div');
+  const toggle = document.createElement('input');
+  addLabel(div, toggle, 'debug', 'Show Potential Field:');
+  toggle.setAttribute('type', 'checkbox');
+  toggle.checked = false;
+  toggle.addEventListener('change', () => {
+    controls.onDebugUpdate(toggle.checked);
+  });
+  div.appendChild(toggle);
+  main.appendChild(div);
+  return {};
+}
+
+function addRemoveAll(
+  /** @type {HTMLElement} */ main,
+  /** @type {Controls} */ controls,
+) {
+  const div = document.createElement('div');
+  const removeAll = document.createElement('input');
+  removeAll.setAttribute('type', 'button');
+  removeAll.value = 'Clear';
+  removeAll.addEventListener('click', () => {
+    controls.onClear();
+  });
+  div.appendChild(removeAll);
+  main.appendChild(div);
+  return {};
+}
+
+export function addControls(/** @type {Controls} */ controls) {
+  const top = document.getElementById('topbar');
+  const presetControls = addPresetSelect(top, controls);
+  const colorControls = addColorSelect(top, controls);
+  const removeControls = addRemoveToggle(top, controls);
+  addDivider(top);
+  const paddingControls = addPaddingSlider(top, controls);
+  const debugControls = addDebugToggle(top, controls);
+  const clearControls = addRemoveAll(top, controls);
+  const footNormal = document.createElement('div');
+  footNormal.classList.add('normalonly');
+  footNormal.textContent =
+    'Add boxes by clicking and remove the currently closest point via Shift+Click.';
+  const footMobile = document.createElement('div');
+  footMobile.classList.add('mobileonly');
+  footMobile.textContent =
+    'Add boxes by tapping. Select "Remove" to remove points instead.';
+  const footer = document.getElementById('footer');
+  footer.appendChild(footNormal);
+  footer.appendChild(footMobile);
+  return {
+    ...presetControls,
+    ...colorControls,
+    ...removeControls,
+    ...paddingControls,
+    ...debugControls,
+    ...clearControls,
+  };
+}
--- index.js
+++ index.js
@@ -15,459 +15,64 @@
  */
 // @ts-check
 
-/** @typedef {import('./bubblesets').RectObj} RectObj */
-/**
- * @typedef {{
- *  x: number,
- *  y: number,
- *  width: number,
- *  height: number,
- *  elem: SVGElement,
- * }} SVGRectObj
- */
-/**
- * @typedef {{
- *  id: string | null,
- *  name: string,
- *  create: (items: SVGElement, rectangles: SVGRectObj[][], width: number, height: number) => void | null,
- * }} Preset
- */
-/**
- * @typedef {{
- *  initPreset: number,
- *  initIx: number,
- *  onPresetUpdate: (preset: Preset) => void,
- *  onColorUpdate: (ix: number) => void,
- *  onDebugUpdate: (isDebug: boolean) => void,
- *  onRemoveUpdate: (isRemove: boolean) => void,
- *  onClear: () => void,
- * }} Controls
- */
-
 import {
   BSplineShapeGenerator,
   BubbleSet,
-  Point,
   PointPath,
-  Rectangle,
   ShapeSimplifier,
 } from './bubblesets.js';
+import { addControls } from './controls.js';
+import { COLORS, PRESETS, addRect, clearAll, removeRect } from './presets.js';
+import {
+  DEFAULT_HEIGHT,
+  DEFAULT_WIDTH,
+  appendSVG,
+  attr,
+  getSize,
+  rectFromPoint,
+  removeAllChilds,
+  style,
+} from './util.js';
 
-function attr(
-  /** @type {HTMLElement | SVGElement} */ elem,
-  /** @type {{ [key: string]: string | number | null }} */ attr,
-) {
-  for (const key of Object.keys(attr)) {
-    const value = attr[key];
-    if (value === null) {
-      elem.removeAttribute(key);
-    } else {
-      elem.setAttribute(key, `${value}`);
-    }
-  }
-}
-
-function style(
-  /** @type {HTMLElement | SVGElement} */ elem,
-  /** @type {{ [key: string]: string | number | null }} */ style,
-) {
-  for (const key of Object.keys(style)) {
-    const value = style[key];
-    if (value === null) {
-      elem.style.removeProperty(key);
-    } else {
-      elem.style.setProperty(key, `${value}`);
-    }
-  }
-}
-
-function appendSVG(
-  /** @type {HTMLElement | SVGElement} */ parent,
-  /** @type {string} */ name,
-) {
-  return parent.appendChild(
-    document.createElementNS('http://www.w3.org/2000/svg', name),
-  );
-}
-
-function removeAllChilds(/** @type {SVGElement} */ parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
-
-function getSize(/** @type {HTMLElement | SVGElement} */ elem) {
-  let width = elem.clientWidth;
-  let height = elem.clientHeight;
-  if (width === 0 || height === 0) {
-    // taking care of firefox
-    const bbox = elem.getBoundingClientRect();
-    width = bbox.width;
-    height = bbox.height;
-  }
-  return [width, height];
-}
-
-function centerPointList(
-  /** @type {number[][][]} */ points,
-  /** @type {number} */ width,
-  /** @type {number} */ height,
-) {
-  const [sx, sy, total] = points.reduce(
-    (prev, ps) => {
-      return ps.reduce(
-        (pv, p) => [pv[0] + p[0], pv[1] + p[1], pv[2] + 1],
-        [prev[0], prev[1], prev[2]],
-      );
-    },
-    [0, 0, 0],
-  );
-  return points.map((ps) => {
-    return ps.map((p) => {
-      return [
-        p[0] + width / 2 - sx / total - DEFAULT_WIDTH / 2,
-        p[1] + height / 2 - sy / total - DEFAULT_HEIGHT / 2,
-      ];
-    });
-  });
-}
-
-function fromPointList(
-  /** @type {SVGElement} */ items,
-  /** @type {SVGRectObj[][]} */ rectangles,
-  /** @type {number[][][]} */ points,
-) {
-  points.forEach((colorPoints, ix) => {
-    if (ix >= colors.length) {
-      return;
-    }
-    colorPoints.forEach((point) => {
-      addRect(items, rectangles[ix], colors[ix], {
-        x: point[0],
-        y: point[1],
-        width: DEFAULT_WIDTH,
-        height: DEFAULT_HEIGHT,
-      });
-    });
-  });
-}
-
-const colors = [
-  '#377eb8',
-  '#e41a1c',
-  '#4daf4a',
-  '#984ea3',
-  '#ff7f00',
-  '#ffff33',
-  '#a65628',
-  '#f781bf',
-  '#999999',
-];
-
-/** @type {Preset[]} */
-const presets = [
-  {
-    id: null,
-    name: 'Custom',
-    create: null,
-  },
-  {
-    id: 'bigb',
-    name: 'Big B',
-    create: (items, rectangles, width, height) => {
-      const points = [
-        [
-          [498, 142],
-          [503, 241],
-          [494, 36],
-        ],
-        [
-          [495, 346],
-          [496, 469],
-          [536, 255],
-        ],
-        [
-          [526, 471],
-          [680, 340],
-          [608, 468],
-          [670, 416],
-        ],
-        [
-          [662, 314],
-          [575, 257],
-          [650, 207],
-        ],
-        [
-          [683, 97],
-          [610, 46],
-          [533, 31],
-          [688, 190],
-        ],
-      ];
-      fromPointList(items, rectangles, centerPointList(points, width, height));
-    },
-  },
-];
-
-function addDivider(/** @type {HTMLElement} */ main) {
-  const div = document.createElement('div');
-  div.classList.add('divider');
-  main.appendChild(div);
-}
-
-function addLabel(
-  /** @type {HTMLElement} */ main,
-  /** @type {HTMLElement} */ target,
-  /** @type {string} */ name,
-  /** @type {string} */ desc,
-) {
-  const label = document.createElement('label');
-  label.setAttribute('for', name);
-  label.textContent = desc;
-  main.appendChild(label);
-  attr(target, { id: name, name });
-}
-
-function addPresetSelect(
-  /** @type {HTMLElement} */ main,
-  /** @type {Controls} */ controls,
-) {
-  const div = document.createElement('div');
-  const selectPreset = document.createElement('select');
-  addLabel(div, selectPreset, 'select-preset', 'Preset:');
-  presets.forEach((preset, ix) => {
-    const option = document.createElement('option');
-    option.value = `${ix}`;
-    option.innerText = preset.name;
-    selectPreset.appendChild(option);
-  });
-  selectPreset.addEventListener('change', () => {
-    const newIx = +selectPreset.value;
-    controls.onPresetUpdate(presets[newIx]);
-  });
-  selectPreset.value = `${controls.initPreset}`;
-  div.appendChild(selectPreset);
-  main.appendChild(div);
-  return {
-    updatePreset: (/** @type {number} */ ix) => {
-      if (selectPreset.value !== `${ix}`) {
-        selectPreset.value = `${ix}`;
-      }
-    },
-  };
-}
-
-function addColorSelect(
-  /** @type {HTMLElement} */ main,
-  /** @type {Controls} */ controls,
-) {
-  const div = document.createElement('div');
-  const selectColor = document.createElement('select');
-  addLabel(div, selectColor, 'select-color', 'Color:');
-  colors.forEach((_, ix) => {
-    const option = document.createElement('option');
-    option.value = `${ix}`;
-    option.innerText = `Group ${ix}`;
-    selectColor.appendChild(option);
-  });
-  selectColor.addEventListener('change', () => {
-    const newIx = +selectColor.value;
-    selectColor.style.backgroundColor = colors[newIx];
-    controls.onColorUpdate(newIx);
-  });
-  selectColor.value = `${controls.initIx}`;
-  selectColor.style.backgroundColor = colors[controls.initIx];
-  div.appendChild(selectColor);
-  main.appendChild(div);
-  return {
-    updateColor: (/** @type {number} */ ix) => {
-      if (selectColor.value !== `${ix}`) {
-        selectColor.value = `${ix}`;
-      }
-    },
-  };
-}
-
-function addRemoveToggle(
-  /** @type {HTMLElement} */ main,
-  /** @type {Controls} */ controls,
-) {
-  const div = document.createElement('div');
-  const toggle = document.createElement('input');
-  addLabel(div, toggle, 'remove', 'Remove:');
-  toggle.setAttribute('type', 'checkbox');
-  toggle.checked = false;
-  toggle.addEventListener('change', () => {
-    controls.onRemoveUpdate(toggle.checked);
-  });
-  div.appendChild(toggle);
-  main.appendChild(div);
-  return {
-    setRemoveMode: (/** @type {boolean} */ isRemove) => {
-      toggle.checked = isRemove;
-    },
-  };
-}
-
-function addDebugToggle(
-  /** @type {HTMLElement} */ main,
-  /** @type {Controls} */ controls,
-) {
-  const div = document.createElement('div');
-  const toggle = document.createElement('input');
-  addLabel(div, toggle, 'debug', 'Show Potential Field:');
-  toggle.setAttribute('type', 'checkbox');
-  toggle.checked = false;
-  toggle.addEventListener('change', () => {
-    controls.onDebugUpdate(toggle.checked);
-  });
-  div.appendChild(toggle);
-  main.appendChild(div);
-  return {};
-}
-
-function addRemoveAll(
-  /** @type {HTMLElement} */ main,
-  /** @type {Controls} */ controls,
-) {
-  const div = document.createElement('div');
-  const removeAll = document.createElement('input');
-  removeAll.setAttribute('type', 'button');
-  removeAll.value = 'Clear';
-  removeAll.addEventListener('click', () => {
-    controls.onClear();
-  });
-  div.appendChild(removeAll);
-  main.appendChild(div);
-  return {};
-}
-
-function addControls(/** @type {Controls} */ controls) {
-  const top = document.getElementById('topbar');
-  const presetControls = addPresetSelect(top, controls);
-  const colorControls = addColorSelect(top, controls);
-  const removeControls = addRemoveToggle(top, controls);
-  addDivider(top);
-  const debugControls = addDebugToggle(top, controls);
-  const clearControls = addRemoveAll(top, controls);
-  const footNormal = document.createElement('div');
-  footNormal.classList.add('normalonly');
-  footNormal.textContent =
-    'Add boxes by clicking and remove the currently closest point via Shift+Click.';
-  const footMobile = document.createElement('div');
-  footMobile.classList.add('mobileonly');
-  footMobile.textContent =
-    'Add boxes by tapping. Select "Remove" to remove points instead.';
-  const footer = document.getElementById('footer');
-  footer.appendChild(footNormal);
-  footer.appendChild(footMobile);
-  return {
-    ...presetControls,
-    ...colorControls,
-    ...removeControls,
-    ...debugControls,
-    ...clearControls,
-  };
-}
-
-const DEFAULT_WIDTH = 40;
-const DEFAULT_HEIGHT = 30;
-
-function rectFromPoint(/** @type {number} */ cx, /** @type {number} */ cy) {
-  const width = DEFAULT_WIDTH;
-  const height = DEFAULT_HEIGHT;
-  const x = cx - width * 0.5;
-  const y = cy - height * 0.5;
-  return {
-    x,
-    y,
-    width,
-    height,
-  };
-}
-
-function addRect(
-  /** @type {SVGElement} */ items,
-  /** @type {SVGRectObj[]} */ rectangles,
-  /** @type {string} */ color,
-  /** @type {RectObj} */ rect,
-) {
-  const elem = appendSVG(items, 'rect');
-  attr(elem, rect);
-  style(elem, {
-    stroke: 'black',
-    'stroke-width': `${1}`,
-    fill: color,
-  });
-  rectangles.push({
-    ...rect,
-    elem: elem,
-  });
-}
-
-function removeRect(
-  /** @type {SVGElement} */ items,
-  /** @type {SVGRectObj[][]} */ rectangles,
-  /** @type {number} */ x,
-  /** @type {number} */ y,
-) {
-  const point = new Point(x, y);
-  const ref = new Rectangle();
-  for (let ix = 0; ix < rectangles.length; ix += 1) {
-    rectangles[ix] = rectangles[ix].filter((rect) => {
-      ref.rect(rect);
-      if (!ref.contains(point)) {
-        return true;
-      }
-      items.removeChild(rect.elem);
-      return false;
-    });
-  }
-}
-
-function clearAll(
-  /** @type {SVGElement} */ items,
-  /** @type {SVGRectObj[][]} */ rectangles,
-) {
-  removeAllChilds(items);
-  colors.forEach((_, ix) => {
-    rectangles[ix] = [];
-  });
-}
+/** @typedef {import("./presets").Preset} Preset */
+/** @typedef {import("./util").SVGRectObj} SVGRectObj */
+/** @typedef {import("./util").SVGLineObj} SVGLineObj */
+/** @typedef {import("./util").RectState} RectState */
+/** @typedef {import("./util").LineState} LineState */
 
 function parseData(
   /** @type {SVGElement} */ svg,
-  /** @type {SVGElement} */ items,
-  /** @type {SVGRectObj[][]} */ rectangles,
+  /** @type {RectState} */ rectState,
+  /** @type {LineState} */ lineState,
   /** @type {number} */ defaultPresetIx,
   /** @type {string | null} */ presetId,
   /** @type {string | null} */ data,
 ) {
-  clearAll(items, rectangles);
-  const presetIx = presets.findIndex(
+  clearAll(rectState, lineState);
+  const presetIx = PRESETS.findIndex(
     (preset) => preset.id && preset.id === presetId,
   );
-  const preset = presetIx >= 0 ? presets[presetIx] : null;
+  const preset = presetIx >= 0 ? PRESETS[presetIx] : null;
   if (preset?.create) {
     const [width, height] = getSize(svg);
-    preset.create(items, rectangles, width, height);
+    preset.create(rectState, lineState, width, height);
     return presetIx;
   }
   if (!data) {
-    const defaultPreset = presets[defaultPresetIx];
+    const defaultPreset = PRESETS[defaultPresetIx];
     if (defaultPreset.create) {
       const [width, height] = getSize(svg);
-      defaultPreset.create(items, rectangles, width, height);
+      defaultPreset.create(rectState, lineState, width, height);
     }
     return defaultPresetIx;
   }
   JSON.parse(atob(data)).forEach(
     (/** @type {number[][]} */ rects, /** @type {number} */ ix) => {
-      if (ix >= rectangles.length) {
+      if (ix >= rectState.rectangles.length) {
         return;
       }
       rects.forEach((r) =>
-        addRect(items, rectangles[ix], colors[ix], {
+        addRect(rectState, ix, COLORS[ix], {
           x: +r[0],
           y: +r[1],
           width: +(r[2] ?? DEFAULT_WIDTH),
@@ -553,31 +158,44 @@ function isTextTarget(/** @type {HTMLElement} */ target) {
 
 function start() {
   const defaultPreset = 1;
+  const defaultPad = 5;
   let curColor = 0;
   let removeMode = false;
-  /** @type {SVGRectObj[][]} */
-  const rectangles = colors.map(() => []);
+  let pad = defaultPad;
   const bubbles = new BubbleSet();
   const main = document.getElementById('main');
   const svg = appendSVG(main, 'svg');
   const full = appendSVG(svg, 'rect');
   attr(full, { x: '0', y: '0', width: '100%', height: '100%' });
   style(full, { fill: 'none' });
-  const items = appendSVG(svg, 'g');
-  const paths = rectangles.map(() => appendSVG(svg, 'path'));
+  /** @type {RectState} */
+  const rectState = {
+    items: appendSVG(svg, 'g'),
+    rectangles: COLORS.map(() => []),
+  };
+  /** @type {LineState} */
+  const lineState = {
+    elems: appendSVG(svg, 'g'),
+    lines: COLORS.map(() => []),
+  };
+  const paths = COLORS.map(() => appendSVG(svg, 'path'));
   const debug = appendSVG(svg, 'g');
   bubbles.debug(false);
 
   const controls = addControls({
     initPreset: defaultPreset,
     initIx: curColor,
+    initPad: defaultPad,
+    minPad: 0,
+    maxPad: 10,
+    stepPad: 1,
     onPresetUpdate: (preset) => {
       if (preset.create) {
-        clearAll(items, rectangles);
+        clearAll(rectState, lineState);
         const [width, height] = getSize(svg);
-        preset.create(items, rectangles, width, height);
+        preset.create(rectState, lineState, width, height);
         update();
-        updateURL(preset, rectangles);
+        updateURL(preset, rectState.rectangles);
       }
     },
     onColorUpdate: (ix) => {
@@ -587,19 +205,23 @@ function start() {
     onRemoveUpdate: (isRemove) => {
       removeMode = isRemove;
     },
+    onPadUpdate: (newPad) => {
+      pad = newPad;
+      update();
+    },
     onDebugUpdate: (isDebug) => {
       bubbles.debug(isDebug);
       update();
     },
     onClear: () => {
-      clearAll(items, rectangles);
+      clearAll(rectState, lineState);
       update();
-      updateURL(presets[0], rectangles);
+      updateURL(PRESETS[0], rectState.rectangles);
     },
   });
 
   function restangles(/** @type {number} */ ix) {
-    return rectangles.flatMap((cur, curIx) => {
+    return rectState.rectangles.flatMap((cur, curIx) => {
       if (curIx === ix) {
         return [];
       }
@@ -609,33 +231,40 @@ function start() {
 
   function update() {
     removeAllChilds(debug);
-    rectangles.forEach((rectangles, ix) => {
-      updateOutline(rectangles, restangles(ix), colors[ix], paths[ix], ix);
+    rectState.rectangles.forEach((rects, ix) => {
+      updateOutline(
+        rects,
+        restangles(ix),
+        lineState.lines[ix],
+        COLORS[ix],
+        paths[ix],
+        ix,
+      );
     });
   }
 
   function updateOutline(
     /** @type {SVGRectObj[]} */ rectangles,
     /** @type {SVGRectObj[]} */ otherRectangles,
+    /** @type {SVGLineObj[]} */ lines,
     /** @type {string} */ color,
     /** @type {SVGElement} */ path,
     /** @type {number} */ ix,
   ) {
-    const pad = 5;
-    const list = bubbles.createOutline(
-      BubbleSet.addPadding(rectangles, pad),
-      BubbleSet.addPadding(otherRectangles, pad),
-      null /* lines */,
-    );
     // rectangles need to have the form { x: 0, y: 0, width: 0, height: 0 }
     // lines need to have the form { x1: 0, x2: 0, y1: 0, y2: 0 }
     // lines can be null to infer lines between rectangles automatically
+    const list = bubbles.createOutline(
+      BubbleSet.addPadding(rectangles, pad),
+      BubbleSet.addPadding(otherRectangles, pad),
+      lines.length ? lines : null,
+    );
+    // outline is a path that can be used for the attribute d of a path element
     const outline = new PointPath(list).transform([
       new ShapeSimplifier(0.0),
       new BSplineShapeGenerator(),
       new ShapeSimplifier(0.0),
     ]);
-    // outline is a path that can be used for the attribute d of a path element
     attr(path, {
       d: `${outline}`,
       opacity: '0.5',
@@ -671,18 +300,14 @@ function start() {
   svg.addEventListener('click', (e) => {
     const ix = curColor;
     if (removeMode) {
-      removeRect(items, rectangles, e.offsetX, e.offsetY);
+      removeRect(rectState, e.offsetX, e.offsetY);
     } else {
-      addRect(
-        items,
-        rectangles[ix],
-        colors[ix],
-        rectFromPoint(e.offsetX, e.offsetY),
-      );
+      addRect(rectState, ix, COLORS[ix], rectFromPoint(e.offsetX, e.offsetY));
     }
+    clearAll(null, lineState);
     controls.updatePreset(0);
     update();
-    updateURL(presets[0], rectangles);
+    updateURL(PRESETS[0], rectState.rectangles);
   });
 
   window.addEventListener('keydown', (e) => {
@@ -695,9 +320,11 @@ function start() {
     }
     const matches = /Digit([0-9])/.exec(e.code);
     if (matches) {
-      const digit = +matches[1];
-      if (digit < rectangles.length) {
+      const digit = +matches[1] - 1;
+      if (digit >= 0 && digit < COLORS.length) {
         controls.updateColor(digit);
+        curColor = digit;
+        update();
         if (target && target.blur) {
           target.blur();
         }
@@ -730,8 +357,8 @@ function start() {
     try {
       const newPresetIx = parseData(
         svg,
-        items,
-        rectangles,
+        rectState,
+        lineState,
         defaultPreset,
         presetId,
         data,
